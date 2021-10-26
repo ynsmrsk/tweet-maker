2f71649571c5aa5ccf5a4f3abf2a8a4a83589476
Yunus Emre <yunusemre@pop-os.localdomain>
2021-10-26 13:59:40 +0300

    Add more language variables.

--- src/language.js
+++ src/language.js
@@ -1,9 +1,26 @@
 const en = {
-	settings: "Tweet Settings"
+	settings: "Tweet Settings",
+	name: "Name Surname",
+	username: "Username",
+	quoteTweets: "Quote Tweets",
+	likes: "Likes",
+	isVerified: "Is Verified",
+	fetchLastTweet: "Fetch last tweet",
 }
 
 const tr = {
-	settings: "Tweet Ayarları"
+	settings: "Tweet Ayarları",
+	name: "Ad Soyad",
+	username: "Kullanıcı Adı",
+	quoteTweets: "Alıntı Tweetler",
+	likes: "Beğeniler",
+	isVerified: "Onaylı Hesap",
+	fetchLastTweet: "Son tweeti çek",
 }
 
-export {en, tr}
+const language = {
+	en,
+	tr
+}
+
+export { language }
